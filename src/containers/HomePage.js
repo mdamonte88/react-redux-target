91b6ddae7adccd9edd5d14d51b8bd0a2f8662085
Matías Damonte <matiasdamonte@Mac-mini-de-Matias.local>
2018-11-14 15:05:25 -0300

    Add the reducer and components to the HomePage

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,21 +1,56 @@
-import React from 'react';
-
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import Menu from 'components/common/Menu';
 import LogoutButton from 'components/user/LogoutButton';
-import TargetPoint from 'components/user/TargetPoint';
+import SimpleMap from 'components/common/maps/Map';
 import Welcome from 'components/user/Welcome';
+import { array, func } from 'prop-types';
 
-const HomePage = () => (
-  <div className="slidesContainer homepage">
-    <div className="slide col-6">
-      <Welcome currentPage="Home" />
-      <div className="content">
-        <LogoutButton className="sign-in-button" />
+import { loadTargets } from '../actions/targetActions';
+import { loadTopics } from '../actions/topicActions';
+
+class HomePage extends PureComponent {
+  componentDidMount() {
+    this.props.loadTargets();
+    this.props.loadTopics();
+  }
+
+  render() {
+    const { targetList, topicList } = this.props;
+
+    return (
+      <div className="slidesContainer homepage">
+        <Menu />
+        <div className="slide col-6">
+          <Welcome currentPage="Home" />
+          <div className="content">
+            <LogoutButton className="sign-in-button" />
+          </div>
+        </div>
+        <div className="slide col-6">
+          <SimpleMap markers={targetList} topics={topicList} />
+        </div>
       </div>
-    </div>
-    <div className="slide col-6">
-      <TargetPoint />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
-export default HomePage;
+HomePage.propTypes = {
+  targetList: array,
+  topicList: array,
+  loadTargets: func,
+  loadTopics: func
+};
+
+const mapState = state => ({
+  authenticated: state.getIn(['session', 'authenticated']),
+  topicList: state.getIn(['topic', 'topicList']),
+  targetList: state.getIn(['target', 'targetList'])
+});
+
+const mapDispatch = dispatch => ({
+  loadTargets: () => dispatch(loadTargets()),
+  loadTopics: () => dispatch(loadTopics())
+});
+
+export default connect(mapState, mapDispatch)(HomePage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as form } from 'redux-form/immutable';
 import { sessionImmutableReducer as session } from 'redux-react-session';
+import target from './targetReducer';
+import topic from './topicReducer';
 
 import router from './routerReducer';
 
 const rootReducer = combineReducers({
   form,
   session,
-  router
+  router,
+  target,
+  topic
 });
 
 export default rootReducer;
