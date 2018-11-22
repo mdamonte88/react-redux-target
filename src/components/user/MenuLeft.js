a84ee3d2916dadc88a455224b5575545786b8b41
Matias Damonte <mdamonte88@gmail.com>
2018-11-22 17:37:56 -0300

    Generated a new component to show the menuLeft in About and CreateTarget's views

---
+++ src/components/user/MenuLeft.js
@@ -0,0 +1,46 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+import LogoutButton from 'components/user/LogoutButton';
+import Welcome from 'components/user/Welcome';
+import CreateTargetForm from 'components/user/CreateTargetForm';
+import AboutTarget from 'components/user/AboutTarget';
+import routes from 'constants/routesPaths';
+
+export default class MenuLeft extends PureComponent {
+  static propTypes = {
+    title: PropTypes.string,
+    topicList: PropTypes.array,
+    section: PropTypes.string,
+    handleCreateTarget: PropTypes.func,
+  };
+
+  render() {
+    const { topicList, section, title, handleCreateTarget } = this.props;
+    let menu;
+
+    if (section === 'newTarget' || section === 'aboutTarget') {
+      menu = (
+        <div className="slide slideLeft col-6">
+          <div className="headerContent">
+            <FormattedMessage id={title} /> <Link to={routes.index} > <div className="closeIcon" /> </Link>
+          </div>
+          <div className="content createTarget">
+            {section === 'newTarget' ? <CreateTargetForm onSubmit={handleCreateTarget} topics={topicList} /> : <AboutTarget /> }
+          </div>
+        </div>
+      );
+    } else {
+      menu = (
+        <div className="slide slideLeft col-6">
+          <Welcome currentPage="Home" />
+          <div className="content">
+            <LogoutButton className="sign-in-button" />
+          </div>
+        </div>
+      );
+    }
+    return menu;
+  }
+}
